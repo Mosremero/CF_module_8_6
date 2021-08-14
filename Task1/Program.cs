445577a9003a7dac3eed953d0db719d41329900e
Zakharov Ivan <zakharov_ii@proitr.ru>
2021-08-14 17:41:28 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            FolderOper folder = new FolderOper("C:/Users/mosre/Documents/folderDeleteTest");
+            folder.FolderAndFileDelete(30);
+        }
+    }
+}
