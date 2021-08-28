133050fb26ec4de7e84643b5a92fc24159a090aa
Zakharov Ivan <mosremero@gmail.com>
2021-08-28 21:42:37 +0300

    1

---
+++ Task4/BinaryWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Task4
+{
+    class BinaryWork
+    {
+        const string inputPath = "C:/Users/mosre/Downloads/Students.bin";
+        const string outputPath = "C:/Users/mosre/Downloads/Students";
+
+        static void Main()
+        {
+            Student student = new Student();
+            student.ReadValues();
+        }
+
+    }
+}
---
+++ Task4/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Task4
+{
+    class Student
+    {
+        string parseText;
+        public string url = "C:/Users/mosre/Downloads/Students.dat";
+        public void ReadValues()
+        {
+            if (File.Exists(url))
+            {
+                
+                using (BinaryReader br = new BinaryReader(File.Open(url, FileMode.Open)))
+                {
+                  
+                    var myString = Encoding.Default.GetString(br.ReadBytes(4096));
+                    parseText = myString;
+                }
+
+                Console.WriteLine(parseText);
+            }
+
+        }
+    }
+}
